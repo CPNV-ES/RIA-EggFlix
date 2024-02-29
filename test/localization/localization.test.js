db73484b3f5cbd92fad98527455608bb542ea29e
JuilletMikael <mikael.juillet@eduvaud.ch>
2024-02-29 11:56:54 +0100

    Test : add test for localization

---
+++ test/localization/localization.test.js
@@ -0,0 +1,30 @@
+/**
+ * @file      localization.test.js
+ * @brief     This class is designed to test the behaviour of a localization.
+ * @author    Created by Mikael Juillet
+ * @version   29-02-2024 - original
+ */
+
+describe('Localization', () => {
+
+    beforeEach(() => {
+        document.body.innerHTML = '<p data-i18n="Movies" id="movies">Movies</p>';
+        originalElement = document.getElementById('movies').innerText;
+    });
+
+    afterEach(() => {
+        document.body.innerHTML = '';
+    });
+
+    test('User can change localization in french', () => {
+        //given
+
+        //when
+        changeLanguage('fr');
+
+        //then
+        const updatedElement = document.getElementById('movies');
+        expect(updatedElement.innerText).not.toBe(originalElement);
+
+    });
+});
