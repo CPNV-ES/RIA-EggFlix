0cffc6116009ab4070114c10fa0191613ecf6387
JuilletMikael <mikael.juillet@eduvaud.ch>
2024-03-21 10:49:29 +0100

    feat : add login controller

---
+++ public/js/socialNetworks/loginController.js
@@ -0,0 +1,12 @@
+//const LoginService = require("./loginService.js");
+
+class LoginController {
+    //#service = new LoginService();
+    onClickOnLoginLink() {
+
+    }
+
+    onClickOnLoginButton(serviceName) {
+       // this.#service.login(serviceName);
+    }
+}
