cb94d09500312df546b4563a5d58317e074044e9
MikaelJuillet <juillet.mikael@gmail.com>
2024-03-23 18:21:51 +0100

    feat : implement login controller to login user

--- public/js/socialNetworks/loginController.js
+++ public/js/socialNetworks/loginController.js
@@ -1,12 +1,28 @@
-//const LoginService = require("./loginService.js");
-
+import LoginService from "./loginService.js";
+import FacebookLogin from "./facebookLogin.js";
 class LoginController {
-    //#service = new LoginService();
-    onClickOnLoginLink() {
+    #service; //why should it be an array on the diagram ?
 
+    constructor(loginService) { //so we don't show constructor in our diagram ?
+        this.#service = loginService;
+        this.onClickOnLoginLink();
     }
 
-    onClickOnLoginButton(serviceName) {
-       // this.#service.login(serviceName);
+    onClickOnLoginLink() {
+        document.getElementById("loginFacebook").addEventListener(
+            'click',
+            () => { this.onClickOnLoginButton('facebook') }
+        )
+    }
+
+    async onClickOnLoginButton(serviceName) {
+        try {
+           await this.#service.login(serviceName)
+        }catch (e){
+            console.log(e)
+        }
+        window.location.href = 'index.html';
     }
 }
+
+new LoginController(new LoginService([ new FacebookLogin()])); //is this legit ?
--- public/js/socialNetworks/loginService.js
+++ public/js/socialNetworks/loginService.js
@@ -1,9 +1,14 @@
-module.exports = class LoginService{
+export default class LoginService{
+    #socialNetworksLogin
     constructor(socialLoginList) {
+        this.#socialNetworksLogin = socialLoginList;
     }
 
     async login(serviceName){
 
+        this.#socialNetworksLogin.forEach(async (social) => {
+            if (serviceName === social.getSocialNetworkName()) await social.login()
+        })
     }
 
     async isConnectedToAny(){
