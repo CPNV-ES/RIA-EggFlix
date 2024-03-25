92693b9cdbb9dd2923e647991d3ef11c4011074f
Mikael Juillet <mikael.juillet@eduvaud.ch>
2024-03-25 08:08:50 +0100

    feat : add login facebook

--- public/js/socialNetworks/facebookLogin.js
+++ public/js/socialNetworks/facebookLogin.js
@@ -1,17 +1,28 @@
 module.exports = class FacebookLogin {
     constructor() {
-
+        FB.init({
+            appId      : '{app-id}',
+            cookie     : true,                     // Enable cookies to allow the server to access the session.
+            xfbml      : true,                     // Parse social plugins on this webpage.
+            version    : '{api-version}'           // Use this Graph API version for this call.
+        })
     }
     async isConnected(){
-
+        FB.getLoginStatus()
     }
     async login(){
-        FB.login()
+        FB.login(function(response) {
+            if (response.status === 'connected') {
+               return response
+            } else {
+                throw new Error(response)
+            }
+        });
     }
     async logout(){
 
     }
-    getSocialNetworkName(){
+    async getSocialNetworkName(){
 
     }
 }
