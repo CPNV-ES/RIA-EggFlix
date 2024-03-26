a0775437a475425c1d9b08731b56e2051af7373b
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2024-03-26 09:35:31 +0100

    test : merge new user experience driver

--- test/userExperience.js
+++ test/userExperience.js
@@ -1,17 +1,19 @@
 const {Builder, By, Key, until} = require("selenium-webdriver");
+const {delayed} = require("selenium-webdriver/lib/promise");
 
 module.exports = class UserExperience{
     #driver;
+    #ServerAddress = "http://localhost:8080/"
     async setupDriver() {
         this.#driver = await new Builder().forBrowser('chrome').build();
     }
 
     async goToPage(pageName) {
-        await this.#driver.get("file://" + process.cwd() + "/public/"+pageName);
+        await this.#driver.get(this.getServerAddress() + pageName);
     }
 
-    currentRoute(){
-        return this.#driver.current_url;
+    async currentRoute(){
+        return await this.#driver.getCurrentUrl();
     }
 
     async clickOnLoginLink(){
@@ -39,7 +41,13 @@ module.exports = class UserExperience{
     }
 
     async releaseDriver(){
-        await this.driver.quit();
+        await this.#driver.close();
+        // Some browser's only close with "quit" method, if the "close" don't work try to use "quit"
+        try{
+            await this.#driver.quit();
+        }catch (error){
+            console.log("Browser closed already, " + "did not need to quit after all");
+        }
     }
 
     getLoginRoute(){
@@ -48,6 +56,10 @@ module.exports = class UserExperience{
 
     getHomeRoute(){
         return "index.html";
+    }
+
+    getServerAddress() {
+        return this.#ServerAddress;
     }
 
     async isErrorBoxDisplayed() {
@@ -64,6 +76,10 @@ module.exports = class UserExperience{
 
     async isErrorBoxDisplayed() {
         return await (await this.#getErrorBox()).isDisplayed();
+    }
+
+    async delay1s() {
+        await delayed(1000);
     }
 
     async #getLoginLinkButton(){
