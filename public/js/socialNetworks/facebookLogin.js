61ef363618783a4af668a4b2d757e6710ee37993
JuilletMikael <mikael.juillet@eduvaud.ch>
2024-03-27 16:11:16 +0100

    feat : importing facebook sdk

--- public/js/socialNetworks/facebookLogin.js
+++ public/js/socialNetworks/facebookLogin.js
@@ -1,26 +1,13 @@
 export default class FacebookLogin {
     constructor() {
-        document.addEventListener("DOMContentLoaded", function() {
-            // <!-- FB SDK LOAD START -->
-            window.fbAsyncInit = function() {
-                FB.init({
-                    appId      : 'yourAPPID',
-                    cookie     : true,
-                    xfbml      : true,
-                    version    : 'v5.0'
-                });
-                FB.AppEvents.logPageView();
-            };
-            (function(d, s, id){
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) {return;}
-                js = d.createElement(s); js.id = id;
-                js.src = "https://connect.facebook.net/en_US/sdk.js";
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-            // <!-- FB SDK LOAD END -->
-        });
-    }
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId            : '286848547780935',
+                xfbml            : true,
+                version          : 'v19.0'
+            });
+        };}
+
     async isConnected(){
         return new Promise((resolve, reject) => {
             FB.getLoginStatus(function(response) {
